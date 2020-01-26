7ec209c8fe27097255986e1f79919f28d980183b
cww <chiwawa.str@gmail.com>
2020-01-26 19:21:24 +0900

    modify

--- src/Eratosthenes.hpp
+++ src/Eratosthenes.hpp
@@ -1,12 +1,11 @@
 #include <vector>
 using namespace std;
-struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
 vector<int> Eratosthenes( const int N )
 {
     vector<bool> is_prime( 10000 + 1 );
     for( int i = 0; i <= N; i++ )
     {
-        is_prime[ i ] = true;//初期化
+        is_prime[ i ] = true;
     }
     vector<int> P;
     for( int i = 2; i <= N; i++ )
