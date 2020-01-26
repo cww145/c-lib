be58b07bbe2a2e7a86d3f582b1cc111f2b17c04e
cww <chiwawa.str@gmail.com>
2020-01-26 17:39:24 +0900

    add eratosthenes

---
+++ src/Eratosthenes.hpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct cww{cww(){ios::sync_with_stdio(false);cin.tie(0);}}star;
+//エラトステネスの篩
+vector<int> Eratosthenes( const int N )
+{
+    vector<bool> is_prime( 10000 + 1 );
+    for( int i = 0; i <= N; i++ )
+    {
+        is_prime[ i ] = true;//初期化
+    }
+    vector<int> P;
+    for( int i = 2; i <= N; i++ )
+    {
+        if( is_prime[ i ] )
+        {
+            for( int j = 2 * i; j <= N; j += i )
+            {
+                is_prime[ j ] = false;
+            }
+            P.emplace_back( i );
+        }
+    }
+    return P;
+}
+int main()
+{
+    int N;
+    cin >> N;
+    for( const auto &x: Eratosthenes( N ) )
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
